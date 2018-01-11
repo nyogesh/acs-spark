791e227c8680409236821c7ba8f3552fb5b4c4ac
Yogesh Natarajan <ynataraj@adobe.com>
2018-01-11 12:40:43 -0800

    Update shell script

--- extensions/spark-install/v1/spark_install.sh
+++ extensions/spark-install/v1/spark_install.sh
@@ -1,7 +1,20 @@
+set -e
+
+echo $(date) " - Starting Script"
+
+export PATH=/home/ynataraj/bin:$PATH
 mkdir /home/ynataraj/bin
 cd /home/ynataraj/bin
+
+echo $(date) " - Downloading dcos cli"
 curl -O https://downloads.dcos.io/binaries/cli/linux/x86-64/dcos-1.8/dcos
 chmod +x dcos
+
+echo $(date) " - Set dcos url to localhost"
 dcos config set core.dcos_url http://localhost
+
+echo $(date) " - Install spark"
 dcos package install spark
+
+echo $(date) " - Finished Script"
 
